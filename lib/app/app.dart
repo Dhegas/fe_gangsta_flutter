dbd6be800cef94fadc0857c3bb40db4e31ecf6eb
Dhegas <dhegasalkadicoxx@gmail.com>
2026-04-09 02:44:36 +0800

    feat: Remove admin dashboard and entry point, refactor app structure for customer and merchant roles

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,4 @@
 import 'package:fe_gangsta_flutter/design_system/theme/app_theme.dart';
-import 'package:fe_gangsta_flutter/features/admin/dashboard/presentation/pages/admin_dashboard_page.dart';
 import 'package:fe_gangsta_flutter/features/customer/menu/presentation/pages/customer_menu_digital_page.dart';
 import 'package:fe_gangsta_flutter/features/merchant/pos/presentation/pages/pos_page.dart';
 import 'package:flutter/material.dart';
@@ -12,10 +11,6 @@ class GangstaApp extends StatelessWidget {
   const GangstaApp.merchant({super.key})
     : _home = const PosPage(),
       _title = 'Gangsta Kuliner - Merchant';
-
-  const GangstaApp.admin({super.key})
-    : _home = const AdminDashboardPage(),
-      _title = 'Gangsta Kuliner - Admin';
 
   final Widget _home;
   final String _title;
--- lib/main_admin.dart
+++
@@ -1,6 +0,0 @@
-import 'package:fe_gangsta_flutter/app/app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const GangstaApp.admin());
-}
