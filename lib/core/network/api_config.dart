e41aad46ca079fa1a13aeb10b9288968ef66be67
Dhegas <dhegasalkadicoxx@gmail.com>
2026-06-07 00:40:55 +0800

    feat: implement ApiConfig class for base URL management and URI construction

--- lib/core/network/api_config.dart
+++ lib/core/network/api_config.dart
@@ -2,7 +2,8 @@ class ApiConfig {
   ApiConfig._();
 
   static const String _defaultBaseUrl =
-   'http://localhost:8080';
+      //  'http://localhost:8080';
+      'https://saasgangsta-production.up.railway.app';
 
   static const String baseUrl = String.fromEnvironment(
     'APP_DOMAIN',
